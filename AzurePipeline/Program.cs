1fdb15d6ca5dc355527c1af536c2fdb446202df4
Aleksandar Popov <v-apopov@microsoft.com>
2022-06-09 10:25:36 +0200

    Add project files.

---
+++ AzurePipeline/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzurePipeline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First is just to start!");
+        }
+    }
+}
